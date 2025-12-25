f4ab76b1a90b367ad3a533a672ea6156e5c6bea5
Yayang Ardiansyah <39462527+Iyankz@users.noreply.github.com>
2025-12-25 14:24:06 +0700

    Update checkmk.sh

--- checkmk.sh
+++ checkmk.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# --- Author: Iyankz & Gemini ---
 # --- Variabel ---
 CMK_VERSION="2.4.0p4"
 CMK_DEB="check-mk-raw-${CMK_VERSION}_0.noble_amd64.deb"
@@ -12,7 +13,7 @@ sudo apt update -y
 
 # --- 2. Download Checkmk ---
 if [ ! -f "$CMK_DEB" ]; then
-    echo "Mengunduh paket Checkmk versi ${CMK_VERSION}..."
+    echo "Mengunduh paket Checkmk versi ${CMK_VERSION} oleh Author: Iyankz & Gemini..."
     wget $CMK_URL
 else
     echo "Paket ${CMK_DEB} sudah ada, melewati proses download."
@@ -24,7 +25,6 @@ sudo apt install -y ./"$CMK_DEB"
 
 # --- 4. Membuat Situs Monitoring ---
 echo "Membuat situs OMD baru dengan nama: $SITE_NAME"
-# Memeriksa apakah situs sudah ada untuk menghindari error
 if sudo omd sites | grep -q "$SITE_NAME"; then
     echo "Situs '$SITE_NAME' sudah ada."
 else
@@ -39,8 +39,8 @@ sudo omd start "$SITE_NAME"
 IP_ADDR=$(hostname -I | awk '{print $1}')
 echo "--------------------------------------------------------"
 echo "Instalasi Selesai!"
+echo "Script by: Iyankz & Gemini"
 echo "URL Web UI: http://${IP_ADDR}/${SITE_NAME}/"
 echo "Username: cmkadmin"
 echo "Password: (Lihat pada output 'omd create' di atas)"
 echo "--------------------------------------------------------"
-echo " Script Ini di buat oleh Iyankz dan Gemini"
